dfaaca49046eda0bee82b245b88b38f23b522917
idk <patrick.hingley@live.co.uk>
2022-03-05 22:13:51 +0000

    Added USB socket and boxes with A, B and output symbols. Local routing started

---
+++ inkscape/footprints/UpSwitch.svg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+extra = """  (pad 2 smd rect (at -2.81467 2.25) (size 1.5 1) (layers F.Cu F.Paste F.Mask))
+  (pad 1 smd rect (at -2.81467 -2.25) (size 1.5 1) (layers F.Cu F.Paste F.Mask))
+  (pad 1 smd rect (at 5.18533 -2.25) (size 1.5 1) (layers F.Cu F.Paste F.Mask))
+  (pad 2 smd rect (at 5.18533 2.25) (size 1.5 1) (layers F.Cu F.Paste F.Mask))
+)
+"""
+
+x = sys.stdin.read()
+x = x[:x.rfind(')')] + extra
+
+
+
+print(x, end='')
