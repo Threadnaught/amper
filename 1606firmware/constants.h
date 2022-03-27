fcbb15142ebf9dd4e49a638c1127a10e61d28cd1
idk <patrick.hingley@live.co.uk>
2022-03-27 05:19:42 +0100

    minimum viable power supply (single rail)

---
+++ 1606firmware/constants.h
@@ -0,0 +1,78 @@
+#ifndef __AVR_ATtiny1606__
+#define __AVR_ATtiny1606__
+#endif
+
+#include <avr/pgmspace.h>
+#include <avr/io.h>
+#include <inttypes.h>
+
+struct reg{
+	volatile void* target;
+	uint8_t size;
+	uint32_t val;
+};
+
+#define F_CPU 20000000UL
+
+#define BAUDRATE 9600
+#define BAUD_REG (uint16_t)((float)(F_CPU * 64 / (16 *(float)BAUDRATE)) + 0.5)
+
+//TM1637:
+#define TM1637_DATA	0x40
+#define TM1637_ADDRESS	0xC0
+#define TM1637_DISPLAY	0x80
+#define TM1637_CLOCK_US 10 //100 is a safe value
+#define TM1637_BRIGHTNESS 0x00 //0x00 - 0x0f
+#define TM1637_CLK PIN4
+#define TM1637_DIO PIN5
+#define TM1637_DIGITS 6
+
+const unsigned char segmentOrder[6] PROGMEM = {2,3,5,4,1,0};
+
+// where max voltage is in tenths:
+// measured value / (max value / max voltage)
+// = (measured value << 5) / ((max value <<	5) / max voltage)
+// = (measured value << 5) / ((0x3FF << 5) / 43)
+// ~= (measured value << 5) / 0x02F9
+#define ADC_CONVERSION_CONSTANT 0x02F9
+
+#define DEBOUNCE_CYCLES 75
+
+#define MAX_V 43
+#define MIN_V 0
+#define pwmShift 5
+#define pwmMax 127 << pwmShift
+
+//util funcs:
+#define MAX(a,b) (a>b)?a:b
+#define MIN(a,b) (a<b)?a:b
+#define DIV_ROUND(a,b) (a + (b/2))/b
+
+const struct reg regs[] PROGMEM = {
+	//setup pin directions;
+	{&PORTA_DIR, 8, PIN3_bm},
+	{&PORTB_DIR, 8, PIN0_bm | PIN2_bm},
+	{&PORTC_DIR, 8, 0x00},
+	//UART setup:
+	{&USART0_BAUD, 16, BAUD_REG},
+	{&USART0_CTRLC, 8, USART_CHSIZE0_bm | USART_CHSIZE1_bm},
+	{&USART0_CTRLB, 8, USART_TXEN_bm},//enable always last
+	//ADC setup:
+	{&VREF_CTRLA, 8, VREF_ADC0REFSEL_4V34_gc},
+	{&ADC0_CTRLB, 8, 0x00},
+	{&ADC0_CTRLC, 8, ADC_PRESC0_bm | ADC_PRESC1_bm | ADC_PRESC2_bm},
+	{&ADC0_CTRLD, 8, 0x00},
+	{&ADC0_CTRLE, 8, 0x00},
+	{&ADC0_CALIB, 8, ADC_DUTYCYC_bm},
+	{&ADC0_CTRLA, 8, ADC_ENABLE_bm},//enable always last
+	//PWM setup:
+	{&TCA0_SINGLE_CTRLD, 8, TCA_SINGLE_SPLITM_bm},
+	{&TCA0_SPLIT_LPER, 8, 127},
+	{&TCA0_SPLIT_HPER, 8, 127},
+	{&TCA0_SPLIT_LCMP0, 8, 127},
+	{&TCA0_SPLIT_HCMP0, 8, 127},
+	{&TCA0_SPLIT_CTRLB, 8, TCA_SPLIT_HCMP0EN_bm | TCA_SPLIT_LCMP0EN_bm},
+	{&TCA0_SPLIT_CTRLA, 8, TCA_SPLIT_ENABLE_bm},//enable always last
+	//Terminate:
+	{0x00, 0, 0x00},
+};
--- 1606firmware/updown.c
+++ 1606firmware/updown.c
@@ -2,96 +2,92 @@
 #define __AVR_ATtiny1606__
 #endif
 
+#include "./chars.h"
+#include "./constants.h"
 
-#define F_CPU 20000000UL
 #include <inttypes.h>
 #include <avr/io.h>
 #include <util/delay.h>
-
-#include "./chars.h"
-
-#define LED_DATA	0x40
-#define LED_ADDRESS	0xC0
-#define LED_DISPLAY	0x80
-#define DATA_CLOCK_US 10 //100 is a safe value
-
-//0x00 - 0x0f
-// #define BRIGHTNESS 0x00
-#define BRIGHTNESS 0x0f
-
-//Pins:
-#define TM1637_CLK PIN4
-#define TM1637_DIO PIN5
-
-
 
 #define SOURCE_A_OUT_ADC ADC_MUXPOS_AIN1_gc
 #define SOURCE_B_OUT_ADC ADC_MUXPOS_AIN2_gc
 #define RESULT_ADC ADC_MUXPOS_AIN6_gc
 
-#define DIGITS 6
+inline void uartPutChar(char c){
+	while (!(USART0.STATUS & USART_DREIF_bm));
+	USART0.TXDATAL = c;
+}
+inline void uartPutProgmemStr(const char* pgstr){
+	for(size_t i = 0; pgm_read_byte(&pgstr[i]) != 0; i++)
+		uartPutChar(pgm_read_byte(&pgstr[i]));
+}
+//The fact that arabic numerals come out msb first must have been created specifically to annoy me
+inline void uartPutHexByte(unsigned char val){
+	for(long i = 4; i >= 0; i -= 4){
+		unsigned char cursor = 0x0F << i;
+		unsigned char cur = (val & cursor) >> i;
+		if(cur >= 10){
+			uartPutChar('A' + cur - 10);
+		} else {
+			uartPutChar('0' + cur);
+		}
+	}
+}
 
-#define DEBOUNCE_CYCLES 10
-
-#define MAX_V 43
-#define MIN_V 0
-
-#define MAX(a,b) (a>b)?a:b
-#define MIN(a,b) (a<b)?a:b
-#define DIV_ROUND(a,b) (a + (b/2))/b
-
-const unsigned char segmentOrder[6] PROGMEM = {2,3,5,4,1,0};
+inline void uartPutHexInt(unsigned long val, int bytes){
+	unsigned char* cast = (unsigned char*)&val;
+	for(int i = bytes-1; i >= 0; i--)
+		uartPutHexByte(cast[i]);
+}
 
 //Set the pinmode of TM1637_CLK and TM1637_DIO
 //Output mode (1) has side effect of reducing impedance to GND and so drives the pin LOW
 //Input mode (0) allows pin to be pulled HIGH by external resistors
-static inline void SetTM1637_CLKTM1637_DIOMode(unsigned char TM1637_CLKMode, unsigned char TM1637_DIOMode){
-	
-	if(TM1637_CLKMode)
+static inline void Tm1637Set(unsigned char ClkMode, unsigned char DioMode){
+	if(ClkMode)
 		PORTA_DIR &= ~(1 << TM1637_CLK);
 	else
 		PORTA_DIR |= (1 << TM1637_CLK);
-	if(TM1637_DIOMode)
+	if(DioMode)
 		PORTA_DIR &= ~(1 << TM1637_DIO);
 	else
 		PORTA_DIR |= (1 << TM1637_DIO);
-	_delay_us(DATA_CLOCK_US);
+	_delay_us(TM1637_CLOCK_US);
 }
 
 //start and stop a TM1637 transaction around a block of code
 #define txn(a) { \
-	SetTM1637_CLKTM1637_DIOMode(1, 0); \
+	Tm1637Set(1, 0); \
 	a \
-	SetTM1637_CLKTM1637_DIOMode(0,0);\
-	SetTM1637_CLKTM1637_DIOMode(1,0);\
-	SetTM1637_CLKTM1637_DIOMode(1,1);\
+	Tm1637Set(0,0);\
+	Tm1637Set(1,0);\
+	Tm1637Set(1,1);\
 }
 
 //Bang b out to the TM1637
 void sendByte(unsigned char b){
 	for(unsigned char i = 0; i < 8; i++) {
-		SetTM1637_CLKTM1637_DIOMode(0,1);
+		Tm1637Set(0,1);
 		unsigned char bit = b & 0b00000001;
-		SetTM1637_CLKTM1637_DIOMode(0,bit);
-		SetTM1637_CLKTM1637_DIOMode(1,bit);
+		Tm1637Set(0,bit);
+		Tm1637Set(1,bit);
 		b >>= 1;
 	}
-	SetTM1637_CLKTM1637_DIOMode(0,1);
-	SetTM1637_CLKTM1637_DIOMode(1,1);
-	SetTM1637_CLKTM1637_DIOMode(0,0);
+	Tm1637Set(0,1);
+	Tm1637Set(1,1);
+	Tm1637Set(0,0);
 }
 
-//Assumes segs is DIGITS bytes long
+//Assumes segs is TM1637_DIGITS bytes long
 void sendSegs(const unsigned char* segs){
 	txn({
-		sendByte(LED_ADDRESS);
-		for(unsigned char i = 0; i < DIGITS; i++){
+		sendByte(TM1637_ADDRESS);
+		for(unsigned char i = 0; i < TM1637_DIGITS; i++){
 			// sendByte(segs[i]);
 			sendByte(segs[pgm_read_byte(&segmentOrder[i])]);
 		}
 	});
 }
-
 
 void intToTwoSegs(int i, unsigned char* segs, int tenths){
 	segs[1] 
@@ -102,12 +98,7 @@ void intToTwoSegs(int i, unsigned char* segs, int tenths){
 		| (tenths ? pgm_read_byte(chars +'.') : 0);
 }
 
-unsigned char voltages[] = { 10, 10, 10 };
-unsigned char debounceInputs[] = { 0, 0 }; //could have one global debounce
-
-unsigned char segments[6];
-
-void handleButtons(int i, unsigned char up_mask, unsigned char down_mask){
+inline void handleButtons(int i, unsigned char up_mask, unsigned char down_mask, unsigned char* debounceInputs, unsigned char* voltages){
 	if((PORTC_IN & up_mask) && (debounceInputs[i] == 0)){
 		voltages[i] = MIN(MAX_V, voltages[i]+1);
 		debounceInputs[i]=DEBOUNCE_CYCLES;
@@ -121,74 +112,110 @@ void handleButtons(int i, unsigned char up_mask, unsigned char down_mask){
 	debounceInputs[i] = MAX(0, debounceInputs[i]-1);
 }
 
-unsigned short adcRawCounts(unsigned char muxpos){
+inline unsigned short adcRawCounts(unsigned char muxpos){
 	ADC0_MUXPOS = muxpos;
 
 	ADC0_COMMAND = 0x01;
 
 	while(ADC0_COMMAND);
 
+	// uartPutProgmemStr(PSTR("ADC Result:")); uartPutHexInt(ADC0_RES, 2); uartPutChar('\n');
 	return MIN(ADC0_RES, 0x03FF);
 }
 
-// where max voltage is in tenths:
-// measured value / (max value / max voltage)
-// = (measured value << 5) / ((max value <<  5) / max voltage)
-// = (measured value << 5) / ((0x3FF << 5) / 43)
-// ~= (measured value << 5) / 0x02F9
-
 //TODO: calibration
-
-#define ADC_CONVERSION_CONSTANT 0x02F9
-
-unsigned short adcTenthsVolt(unsigned char muxpos){
-	return DIV_ROUND((adcRawCounts(muxpos) << 5), ADC_CONVERSION_CONSTANT);
+inline unsigned short adcTenthsVolt(unsigned short counts){
+	return DIV_ROUND((counts << 5), ADC_CONVERSION_CONSTANT);
 }
 
-unsigned short tenthsVoltRawCount(unsigned short tenths){
-	return (tenths * ADC_CONVERSION_CONSTANT) >> 6;
+inline unsigned short tenthsVoltRawCount(unsigned short tenths){
+	return (tenths * ADC_CONVERSION_CONSTANT) >> 5;
+}
+
+void initRegs(){
+	const struct reg* r = regs;
+	while(1){
+		struct reg cur;
+		memcpy_P(&cur, r, sizeof(struct reg));
+		switch(cur.size){
+			case 0:
+				return;
+			case 8:
+				*((volatile uint8_t*)cur.target) = (uint8_t)cur.val;
+				break;
+			case 16:
+				*((volatile uint16_t*)cur.target) = (uint16_t)cur.val;
+				break;
+			case 32:
+				*((volatile uint32_t*)cur.target) = (uint32_t)cur.val;
+				break;
+		}
+		r++;
+	}
+}
+
+void handlePwm(){
+	
 }
 
 void main(){
+	//Set clock to maximum (20 MHz)
+	_PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, 0);
 
-	//General I/O setup
-	PORTA_DIR = 0;
-	PORTB_DIR = 0;
-	PORTC_DIR = 0;
+	initRegs();
 
-	//ADC setup:
-	VREF_CTRLA = VREF_ADC0REFSEL_4V34_gc;
-	
-	ADC0_CTRLA = 0x00;
-	ADC0_CTRLB = 0x00;
-	ADC0_CTRLC = 0x00;
-	ADC0_CTRLD = 0x00;// set this to ADC_ASDV_bm to randomise the samples a little
-	ADC0_CTRLE = 0x00;
+	uartPutProgmemStr(PSTR("Set up regs\n"));
 
-	//  Enable adc:
-	ADC0_CTRLA = ADC_ENABLE_bm;
+	unsigned char voltages[] = { 10, 10, 10 };
+	unsigned char debounceInputs[] = { 0, 0 }; //could have one global debounce
+	short pwmOutputs[] = {pwmMax, pwmMax};
+	unsigned char segments[6];
 
-	SetTM1637_CLKTM1637_DIOMode(1,1);
-	//Turn display on and set brightness
+	short pwmOutputB = 127<<5;
+
+	int loops = 0;
+
+	//Turn display on and set TM1637_BRIGHTNESS
+	Tm1637Set(1,1);
 	txn({
-		sendByte(LED_DATA);
+		sendByte(TM1637_DATA);
 	});
 	txn({
-		sendByte( LED_DISPLAY				//command
+		sendByte( TM1637_DISPLAY				//command
 				| 0x08						//display is on
-				| (BRIGHTNESS & 0x07)); 	//set brightness
+				| (TM1637_BRIGHTNESS & 0x07)); 	//set TM1637_BRIGHTNESS
 	});
 
 	while(1){
-		voltages[2] = adcTenthsVolt(SOURCE_B_OUT_ADC);
-		// voltages[2] = adcTenthsVolt(ADC_MUXPOS_AIN6_gc);
-
+		int counts = adcRawCounts(SOURCE_B_OUT_ADC);
+		
 		for(int i = 0; i < 3; i++){
 			intToTwoSegs(voltages[i], segments + (i*2), 1);
 		}
 
-		handleButtons(0, PIN0_bm, PIN1_bm);
-		handleButtons(1, PIN2_bm, PIN3_bm);
+		handleButtons(0, PIN0_bm, PIN1_bm, debounceInputs, voltages);
+		handleButtons(1, PIN2_bm, PIN3_bm, debounceInputs, voltages);
+
+		int target = tenthsVoltRawCount(voltages[1]);
+
+		//ran into annoying bug with integer underflowing:
+		if(counts > target && pwmOutputB < 127<<5){
+			pwmOutputB++;
+		} else if (counts < target && pwmOutputB > 0) {
+			pwmOutputB--;
+		}
+
+		if((loops++) % 100 == 0) {
+			voltages[2] = adcTenthsVolt(adcRawCounts(SOURCE_B_OUT_ADC));
+
+			// uartPutProgmemStr(PSTR("Counts:")); uartPutHexInt(counts, 4); uartPutChar('\n');
+			// uartPutProgmemStr(PSTR("Target:")); uartPutHexInt(target, 4); uartPutChar('\n');
+			// uartPutProgmemStr(PSTR("PWM:")); uartPutHexInt(pwmOutputB, 4); uartPutChar('\n');
+
+			// uartPutChar('\n');
+		}
+
+		TCA0_SPLIT_LCMP0 = pwmOutputB>>5;
 
 		sendSegs(segments);
 		// _delay_ms(25);
